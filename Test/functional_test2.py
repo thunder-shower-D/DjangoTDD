ecf4f35bce23df7d26f7b5a80d429a6ef3156457
Monica <328874285@qq.com>
2023-12-15 15:48:46 +0800

    a

--- Test/functional_test2.py
+++ Test/functional_test2.py
@@ -42,22 +42,39 @@ class NewVisitorTest(unittest.TestCase):
         # 她在一个文本框中输入了“Buy peacock feathers”（购买孔雀羽毛）
         # 伊迪丝的爱好是使用假蝇做饵钓鱼
         imputbox.send_keys('Buy peacock feathers')
-
         # 她按回车键后，页面更新了
         # 待办事项表格中显示了“1: Buy peacock feathers”
         imputbox.send_keys(Keys.ENTER)
         time.sleep(1)
 
         table = self.browser.find_element(By.ID,'id_list_table')
-        rows = table.find_element(By.TAG_NAME,'tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers' for row in rows)
-        )
+        rows = table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 她输入了“Use peacock feathers to make a fly”（使用孔雀羽毛做假蝇）
         # 伊迪丝做事很有条理
+        inputbox = self.browser.find_element(By.ID,'id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # 页面再次更新，清单中显示了这两个待办事项
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_elements(By.TAG_NAME,'tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
+
+        # 伊迪丝想知道这个网站是否会记住她的清单
+        # 她看到网站为她生成了一个唯一的URL
+        # 页面中有一些文字解说这个功能
         self.fail('Finish the test!')
+
+        # 她访问那个URL，发现待办事项清单还在
 
         # 页面再次更新，她的清单中显示了这两个待办事项
 
--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase
 from django.urls import resolve
-from lists.views import home_page
+#from lists.views import home_page
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 # Create your tests here.
+
 
 class HomePageTest(TestCase):
 
@@ -25,6 +26,10 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response,'home.html')
 
 
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
 
 
 
