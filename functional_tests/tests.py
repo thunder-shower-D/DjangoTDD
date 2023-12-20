b23c18a216b8dc15bff50eba15972e8f69d2f08e
Monica <328874285@qq.com>
2023-12-20 18:59:26 +0800

    Use bootstrap ro improve layout

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -10,7 +10,7 @@
 """
 
 from selenium.common.exceptions import  WebDriverException
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -21,7 +21,7 @@ MAX_WAIT =10
 
 
 #继承该类可自动清理测试数据
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Chrome()
 
